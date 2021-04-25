b9a50d6b8efe90e6bd876135c26f51efe51c943f
Michael Sasonker <michaelsas1212@gmail.com>
2021-04-25 03:34:09 +0300

    server ready

--- server/src/db/mongoose.db.js
+++ server/src/db/mongoose.db.js
@@ -1,9 +1,7 @@
 const mongoose = require('mongoose');
 const User = require('../models/user.model');
 
-
 const password = process.env.MONGO_PASSWORD;
-// console.log(password);
 
 const uri =
 	`mongodb+srv://admin:${password}@bank-api.e79zr.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
@@ -15,13 +13,6 @@ mongoose.connect(uri, {
 }).then(() => {
 		console.log("Database connect");
 });
-
-// mongoose.connect('mongodb://127.0.0.1:27017/Bank-API', {
-//     useNewUrlParser: true,
-//     useCreateIndex: true,
-//     useUnifiedTopology: true
-// }).then(() => console.log('Connected to the DB server successfully!'))
-// .catch((err) => console.log(err));
 
 // const u1 = new User({
 //     name: 'michael',
