1503558e16d94db3efb9d8baa829796a97ee788e
Michael Sasonker <michaelsas1212@gmail.com>
2021-04-26 14:34:27 +0300

    update

--- server/db/mongoose.db.js
+++ server/db/mongoose.db.js
@@ -3,8 +3,8 @@ const mongoose = require('mongoose');
 
 const password = process.env.MONGO_PASSWORD;
 
-const uri =
-	`mongodb+srv://admin:${password}@bank-api.e79zr.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+// const uri = 'mongodb://127.0.0.1:27017/MyFirstDatabase';
+const uri = `mongodb+srv://admin:${password}@bank-api.e79zr.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 mongoose.connect(process.env.MONGODB_URI || uri, {
 		useNewUrlParser: true,
 		useUnifiedTopology: true,
